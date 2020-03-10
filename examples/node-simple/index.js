87b61ede0aa9de420c8bdaaefebedb7343113ac5
LarsBreuren <larsbreuren@gmail.com>
2020-03-10 16:20:54 +0100

    New API call + some styling and rendering

--- examples/node-simple/index.js
+++ examples/node-simple/index.js
@@ -1,6 +1,7 @@
 // Require third-party modules
 const express = require('express');
 const request = require('request');
+const fetch = require('node-fetch');
 
 // Config object
 const config = {
@@ -17,51 +18,34 @@ app.set('views', 'views');
 // Tell express to use a 'static' folder
 // If the url matches a file it will send that file
 // Sending something (responding) ends the response cycle
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // Create a home route
 app.get('/', function(req, res) {
+
+		res.render('overview',)
+
+});
+
+app.get('/people', function(req, res) {
 	// Send a plain string using res.send();
-	res.send('Hello world');
+	// res.send('Hello world');
+
+	fetch('https://swapi.co/api/people')
+	.then(res => {
+		console.log(res.status)
+		return res.json()
+	})
+
+	// .then(json => console.log(json));
+
+	.then((json) => {
+		res.render('detail', {
+		  personData: json.results,
+		}); 
+	}) 
 });
 
-// Create a route for our overview page
-app.get('/posts', function(req, res) {
-	request('https://jsonplaceholder.typicode.com/posts', {json: true}, function (err, requestRes, body){
-		if (err) {
-			// We got an error
-			res.send(err);
-		} else {
-			// Render the page using the 'posts' view and our body data
-			res.render('posts', {
-				title: 'Posts', // We use this for the page title, see views/partials/head.ejs
-				postData: body
-			});
-		}
-	});
-});
-
-// Create a route for our detail page
-app.get('/post/:id', function(req, res) {
-	request(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`, {json: true}, function (err, requestRes, body){
-		if (err) {
-			// We got an error
-			res.send(err);
-		} else {
-			// Render the page using the 'post' view and our body data
-			res.render('post', {
-				title: `Post ${req.params.id}`, 
-				postData: body
-			});
-		}
-	});
-});
-
-// Make sure to catch /post to /posts
-app.get('/post', function(req, res) {
-	// Redirect the client using res.redirect (this will create a new request)
-	res.redirect('/posts');
-});
 
 // Actually set up the server
 app.listen(config.port, function() {

b89aba2ecbf72dadd4047ade54c746c0e785cb61
LarsBreuren <larsbreuren@gmail.com>
2020-03-13 10:17:03 +0100

    Now working in docs folder, removed examples

---
+++ docs/index.js
@@ -0,0 +1,106 @@
+// Require third-party modules
+const express = require('express');
+const request = require('request');
+const fetch = require('node-fetch');
+
+// Config object
+const config = {
+	port: 3000
+}
+
+// Create new express app in 'app'
+const app = express();
+// Link the templating engine to the express app
+app.set('view engine', 'ejs');
+// Tell the views engine/ejs where the template files are stored (Settingname, value)
+app.set('views', 'views');
+
+// Tell express to use a 'static' folder
+// If the url matches a file it will send that file
+// Sending something (responding) ends the response cycle
+app.use(express.static(__dirname + '/public'));
+
+// Create a home route
+app.get('/', function(req, res) {
+
+	fetch('https://swapi.co/api/people')
+	.then(res => {
+		return res.json()
+	})
+
+	.then((json) => {
+		res.render('people', {
+		  personData: json.results,
+		}); 
+	}) 
+});
+
+app.get('/people', function(req, res) {
+
+	fetch('https://swapi.co/api/people')
+	.then(res => {
+		return res.json()
+	})
+
+	.then((json) => {
+		res.render('people', {
+		  personData: json.results,
+		}); 
+	}) 
+});
+
+app.get('/vehicles', function(req, res) {
+
+	fetch('https://swapi.co/api/vehicles')
+	.then(res => {
+		return res.json()
+	})
+
+	.then((json) => {
+		res.render('vehicles', {
+		  vehicleData: json.results,
+		}); 
+	}) 
+});
+
+app.get('/planets', function(req, res) {
+
+	fetch('https://swapi.co/api/planets')
+	.then(res => {
+		return res.json()
+	})
+
+	.then((json) => {
+		res.render('planets', {
+		  planetsData: json.results,
+		}); 
+	}) 
+});
+
+app.get('/starships', function(req, res) {
+
+	fetch('https://swapi.co/api/starships')
+	.then(res => {
+		return res.json()
+	})
+
+	.then((json) => {
+		res.render('starships', {
+			starshipsData: json.results,
+		}); 
+	}) 
+});
+
+// Actually set up the server
+app.listen(config.port, function() {
+	console.log(`Application started on port: ${config.port}`);
+});
+
+
+// document.getElementById("people").addEventListener("click", navActive("people")); 
+// document.getElementById("planets").addEventListener("click", navActive("planets")); 
+
+// function navActive(x){
+// 	var element = document.getElementById(x);
+// 	element.classList.toggle("active");
+// }

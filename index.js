e2939abe1021619f3f303ae6aa6eb0faa963c669
LarsBreuren <larsbreuren@gmail.com>
2020-03-23 14:15:12 +0100

    Favicon fix + sw pages

--- index.js
+++ index.js
@@ -16,67 +16,66 @@ app.set('views', 'views');
 // If the url matches a file it will send that file
 // Sending something (responding) ends the response cycle
 app.use(express.static(__dirname + '/public'));
+  
 
 // Create a home route
 app.get('/', function (req, res) {
+			res.render('overview')
+})
 
-	fetch('https://swapi.co/api/people')
-		.then(res => {
-			return res.json()
-		})
+			app.get('/:cato', function (req, res) {
+				
+				let cat = req.params.cato;
+				console.log("catogorie is " + cat);
 
-		.then((json) => {
-			res.render('people', {
-				Data: json.results,
+				fetch('https://swapi.co/api/' + req.params.cato)
+					.then(res => {
+						return res.json()
+					})
+
+					.then((items) => {
+						const itemsWithId = items.results.map(item => {
+							const id = item.url.split(req.params.cato + '/')[1]
+							return {
+								id: id,
+								...item
+							}
+						})
+						res.render(req.params.cato, {
+							Data: itemsWithId,
+						});
+					})
 			});
-		})
-});
 
-app.get('/:cato', function (req, res) {
-	fetch('https://swapi.co/api/'+req.params.cato)
-		.then(res => {
-			return res.json()
-		})
-	
-		.then((items) => {
-			const itemsWithId = items.results.map(item => {
-				const id =  item.url.split(req.params.cato + '/')[1]
-				return {
-					id : id,
-					...item
-				}
-			})	
-			res.render(req.params.cato, {
-				Data: itemsWithId,
-			});			
-		})
-});
+			app.get('/:cato/:id', function (req, res) {
+				let cat = req.params.cato;
+				let id = req.params.id;
+				console.log("Je bekijkt " + id + " uit de catogorie " + cat);
+				fetch("https://swapi.co/api/" + req.params.cato + "/" + req.params.id)
+					.then(res => {
+						return res.json()
+					})
 
-app.get('/:cato/:id', function (req, res) {
-	fetch("https://swapi.co/api/" + req.params.cato + "/" + req.params.id)
-	.then(res => {
-		return res.json()
-	})
-
-	.then((json) => {
-		res.render('details', {
-			details: json,
-		}); 
-	}) 
-});
+					.then((json) => {
+						res.render('details', {
+							details: json,
+							link: req.params.cato,
+						});
+					})
+			});
 
 
 
-// Actually set up the server
-app.listen(port, function () {
-	console.log(`Application started on port: ${port}`);
-});
+			// Actually set up the server
+			app.listen(port, function () {
+				console.log(`Application started on port: ${port}`);
+			});
 
 
-// document.getElementById("people").addEventListener("click", navActive("people")); 
-// document.getElementById("planets").addEventListener("click", navActive("planets")); 
+			// document.getElementById("people").addEventListener("click", navActive("people")); 
+			// document.getElementById("planets").addEventListener("click", navActive("planets")); 
 
-// function navActive(x){
-// 	var element = document.getElementById(x);
-// 	element.classList.toggle("active");
-// }
+			// function navActive(x){
+			// 	var element = document.getElementById(x);
+			// 	element.classList.toggle("active");
+			// }
--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,5 @@
-self.oninstall = function () {
-    this.caches.open('fayeP1v1')
-        .then(function (cache) {
-            cache.addAll([
-                    '/',
-                    'index.html',
-                    '/people'
-                ])
-                .then(function () {
-                    console.log('cached!');
-                })
-                .catch(function (err) {
-                    console.log('err ', err);
-                })
-        })
-}
-
-self.onactivate = function () {
-    console.log('activated!');
-}
-
-self.onfetch = function (event) {
-    event.respondWith (
-        caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response;
-                } else {
-                    return fetch(event.request);
-                }
-            })
-    )
+if('serviceWorker' in navigator) {
+   console.log('Browser supported');
+} else{
+    console.log('browser not supported');
 }

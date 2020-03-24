e969afd2351247ed900759a128fb8c7e3955b6cf
LarsBreuren <larsbreuren@gmail.com>
2020-03-24 15:04:51 +0100

    imporved package.json and sort of working SW

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat-css');
 
-gulp.task('minify', () => {
+gulp.task('buildCss', () => {
   return gulp.src('public/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(concat('style.css'))
     .pipe(gulp.dest('public/css/minified'));
 });
 
-gulp.task('default', function(){
-    gulp.watch('public/css/*.css', gulp.series('minify'))
-    return
+gulp.task('watch', function(){
+    gulp.watch('public/css/*.css', gulp.series('buildCss'))
 });
+

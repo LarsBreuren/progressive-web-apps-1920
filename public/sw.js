0068e439f8e9f13d29714f8ad424634cd717b9ff
LarsBreuren <larsbreuren@gmail.com>
2020-03-30 14:50:21 +0200

    New sw added

--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,93 @@
-const precacheVersion = 1;
-const precacheName = 'precache-v' + precacheVersion;
-const precacheFiles = [
+const CORE_CACHE_VERSION = 'v3'
+const CORE_ASSETS = [
   '/',
   '/offline',
   '/about',
   '/css/minified/style.css',
   '/img/bg.jpg',
   '/img/favicon.ico',
-];
+]
 
-self.addEventListener('install', (e) => {
-  console.log('[ServiceWorker] Installed');
+self.addEventListener('install', event => {
+  console.log('Installing service worker')
 
-  self.skipWaiting();
-
-  e.waitUntil(
-    caches.open(precacheName).then((cache) => {
-      console.log('[ServiceWorker] Precaching files');
-      return cache.addAll(precacheFiles);
-    }) // end caches.open()
-  ); // end e.waitUntil
-});
-
-self.addEventListener('activate', (e) => {
-  console.log('[ServiceWorker] Activated');
-
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(cacheNames.map((thisCacheName) => {
-
-        if (thisCacheName.includes("precache") && thisCacheName !== precacheName) {
-          console.log('[ServiceWorker] Removing cached files from old cache - ', thisCacheName);
-          return caches.delete(thisCacheName);
-        }
-
-      }));
-    }) // end caches.keys()
-  ); // end e.waitUntil
-});
-
-self.addEventListener('fetch', (e) => {
-  console.log('[ServiceWorker] Fetch event for ', e.request.url);
-
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-
-      if(cachedResponse){
-        return cachedResponse;
-      }
-
-      return caches.match("/offline");
-
-      
+  event.waitUntil(
+    caches.open(CORE_CACHE_VERSION).then(function(cache) {
+      return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting());
     })
   );
 });
+
+self.addEventListener('activate', event => {
+  console.log('Activating service worker')
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event: ', event.request.url);
+  if (isCoreGetRequest(event.request)) {
+    console.log('Core get request: ', event.request.url);
+    // cache only strategy
+    event.respondWith(
+      caches.open(CORE_CACHE_VERSION)
+        .then(cache => cache.match(event.request.url))
+    )
+  } else if (isHtmlGetRequest(event.request)) {
+    console.log('html get request', event.request.url)
+    // generic fallback
+    event.respondWith(
+
+      caches.open('html-cache')
+        .then(cache => cache.match(event.request.url))
+        .then(response => response ? response : fetchAndCache(event.request, 'html-cache'))
+        .catch(e => {
+          return caches.open(CORE_CACHE_VERSION)
+            .then(cache => cache.match('/offline'))
+        })
+    )
+  }
+});
+
+function fetchAndCache(request, cacheName) {
+  return fetch(request)
+    .then(response => {
+      if (!response.ok) {
+        throw new TypeError('Bad response status');
+      }
+
+      const clone = response.clone()
+      caches.open(cacheName).then((cache) => cache.put(request, clone))
+      return response
+    })
+}
+
+/**
+ * Checks if a request is a GET and HTML request
+ *
+ * @param {Object} request        The request object
+ * @returns {Boolean}            Boolean value indicating whether the request is a GET and HTML request
+ */
+function isHtmlGetRequest(request) {
+  return request.method === 'GET' && (request.headers.get('accept') !== null && request.headers.get('accept').indexOf('text/html') > -1);
+}
+
+/**
+ * Checks if a request is a core GET request
+ *
+ * @param {Object} request        The request object
+ * @returns {Boolean}            Boolean value indicating whether the request is in the core mapping
+ */
+function isCoreGetRequest(request) {
+  return request.method === 'GET' && CORE_ASSETS.includes(getPathName(request.url));
+}
+
+/**
+ * Get a pathname from a full URL by stripping off domain
+ *
+ * @param {Object} requestUrl        The request object, e.g. https://www.mydomain.com/index.css
+ * @returns {String}                Relative url to the domain, e.g. index.css
+ */
+function getPathName(requestUrl) {
+  const url = new URL(requestUrl);
+  return url.pathname;
+}
